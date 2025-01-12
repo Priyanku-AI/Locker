5ff6faf95311bda0c0c1a52e202ed9352eefb571
Priyanku Saikia <paulsaikia6@gmail.com>
2025-01-13 04:00:18 +0530

    navbar completed as of 13th Jan

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-purple-200 flex justify-between items-center px-4'>
+    <nav className='bg-purple-200 flex justify-between items-center px-4 h-24'>
       <div className="logo font-bold">Locker</div>
         <ul>
-            <li className='flex'>
-                <a href="/">Home</a>
-                <a href="#">About</a>
-                <a href="#">Contact</a>
+            <li className='flex gap-4'>
+                <a className='hover:font-bold' href="/">Home</a>
+                <a className='hover:font-bold' href="#">About</a>
+                <a className='hover:font-bold' href="#">Contact</a>
             </li>
         </ul>
     </nav>
